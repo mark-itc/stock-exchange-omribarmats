e1cb9d7d83841ced02550518268c23c564a31f48
omribarmats <omri.bar@gmail.com>
2022-10-10 13:54:21 +0300

    Milestone-1 Done
    1. Created a website with a search input and a search button
    2. When the button is clicked, the website presents a list of 10 search results from the Free Stock API
    3. Each search result includes the company name and symbol and a relative link to company.html

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+let searchButton = document.getElementById("searchButton").addEventListener('click', Search);
+let loadingSpot = document.getElementById("loading")
+let loadingIndicator = `<div class="d-flex justify-content-center"><div class="spinner-grow text-info" role="status">
+<span class="sr-only">Loading...</span>
+</div></div>`;
+
+function Search(e) {
+    e.preventDefault();
+    let searchTerm = document.getElementById("searchTerm").value;
+    getInstruments(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${searchTerm}&limit=10&exchange=NASDAQ`);
+}
+
+async function getInstruments(url) {
+    showResults();
+    let searchResults = document.getElementById("searchResults");
+    loadingSpot.innerHTML = loadingIndicator;
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        loadingSpot.innerHTML = "";
+        result.forEach(item => {
+            let name = item.name;
+            let symbol = item.symbol;
+
+            searchResults.innerHTML += `<li><a href=/company.html?symbol=${symbol}>${name} (${symbol})</a></li>`;
+        });
+    }
+    catch (err) {
+        throw new Error("ERROR: server error", err);
+    }
+}
+
+function showResults() {
+    let resultsTemplate = document.getElementsByTagName("template")[0];
+    let resultsclon = resultsTemplate.content.cloneNode(true);
+    document.body.appendChild(resultsclon, resultsclon);
+    document.getElementById("searchResults").innerHTML = "";
+}
