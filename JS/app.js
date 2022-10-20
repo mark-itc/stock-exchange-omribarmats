71a1433d7bdd4620240164aa897b392f46ada348
omribarmats <omri.bar@gmail.com>
2022-10-20 18:47:00 +0300

    Milestone-3 Done
    1. Added the company image and the price change to the search results

--- JS/app.js
+++ JS/app.js
@@ -12,17 +12,14 @@ function Search(e) {
 
 async function getInstruments(url) {
     showResults();
-    let searchResults = document.getElementById("searchResults");
     loadingSpot.innerHTML = loadingIndicator;
     try {
         const response = await fetch(url);
         const result = await response.json();
         loadingSpot.innerHTML = "";
         result.forEach(item => {
-            let name = item.name;
             let symbol = item.symbol;
-
-            searchResults.innerHTML += `<li><a href=company.html?symbol=${symbol} target="popup" onclick="window.open('company.html?symbol=${symbol}','popup','width=800,height=600')">${name} (${symbol})</a></li>`;
+           getInstrumentsData(symbol);
         });
     }
     catch (err) {
@@ -35,5 +32,18 @@ function showResults() {
     let resultsclon = resultsTemplate.content.cloneNode(true);
     document.body.appendChild(resultsclon, resultsclon);
     document.getElementById("searchResults").innerHTML = "";
+}
+
+async function getInstrumentsData (symbol) {
+    let searchResults = document.getElementById("searchResults");
+    let url = 'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/' + symbol;
+    let response = await fetch(url);
+    let result = await response.json();
+   
+    let image = result.profile.image;
+    let companyName = result.profile.companyName;
+    let priceChange = result.profile.changesPercentage;
+    
+    searchResults.innerHTML += `<li><a href=company.html?symbol=${symbol} target="popup" onclick="window.open('company.html?symbol=${symbol}','popup','width=800,height=600')"><img src=${image} width="20px" alt=""> ${companyName} (${symbol})<b> ${priceChange} %</b></a></li>`
 }
 
